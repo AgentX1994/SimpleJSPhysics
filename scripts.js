3b0ab5240e505543ac471d776a089b42c9bf84eb
John Asper <agentx1994@gmail.com>
2017-12-19 17:08:53 -0500

    Creates and uses Vec2 class

--- scripts.js
+++ scripts.js
@@ -3,71 +3,118 @@ var ctx = cvs.getContext("2d");
 
 lastFrameTime = null;
 
-function Circle(x=0, y=0, r=1, m=1, vx=0, vy=0)
+function Vec2(x=0, y=0)
 {
     this.x = x;
     this.y = y;
+
+    this.add = function(v2)
+    {
+        return new Vec2(this.x + v2.x, this.y + v2.y);
+    }
+    
+    this.subtract = function(v2)
+    {
+        return new Vec2(this.x - v2.x, this.y - v2.y);
+    }
+    
+    this.scale = function(s)
+    {
+        return new Vec2(this.x*s, this.y*s);
+    }
+    
+    this.dot = function(v2)
+    { 
+        return this.x*v2.x + this.y*v2.y;
+    }
+
+    // 2D cross product only has a z component
+    this.cross = function(v2)
+    {
+        return this.x*v2.y - this.y*v2.x;
+    }
+    
+    this.rotate = function(angle, v)
+    {
+        var x = this.x - vector.x;
+        var y = this.y - vector.y;
+
+        var x_prime = vector.x + ((x*Math.cos(angle)) - (y*Math.sin(angle)));
+        var y_prime = vector.y + ((x*Math.sin(angle)) + (y*Math.cos(angle)));
+
+        return new Vec2(x_prime, y_prime);
+    }
+
+    this.length_squared = function()
+    {
+        return this.x*this.x + this.y*this.y;
+    }
+
+    this.length = function()
+    {
+        return Math.sqrt(this.length_squared());
+    }
+}
+
+function Circle(p = new Vec2(), r=1, m=1, v = new Vec2())
+{
+    this.p = p;
     this.r = r;
     this.m = m;
     this.invm = 1/m;
 
-    this.vx = vx;
-    this.vy = vy;
+    this.v = v;
 
-    this.ax = 0;
-    this.ay = 0;
+    this.a = new Vec2();
     
-    this.netfx = 0;
-    this.netfy = 0;
+    this.netf = new Vec2();
 
-    this.addForce = function(fx, fy) { this.netfx += fx; this.netfy += fy; }
+    this.addForce = function(f) { this.netf = this.netf.add(f); }
 
     this.update = function(delta)
     {
         // Velocity Verlot integration
-        var prevax = this.ax;
-        var prevay = this.ay;
+        var preva = this.a;
 
-        this.x += this.vx*delta + 0.5*this.ax*delta*delta;
-        this.y += this.vy*delta + 0.5*this.ay*delta*delta;
+        // p = p + v*dt + (1/2)*a*dt*dt
+        this.p = this.p.add(this.v.scale(delta).add(this.a.scale(0.5*delta*delta)));
 
-        this.ax = this.netfx*this.invm;
-        this.ay = this.netfy*this.invm;
+        // a = f/m
+        this.a = this.netf.scale(this.invm);
 
-        var avgax = (this.ax + prevax)/2;
-        var avgay = (this.ay + prevay)/2;
+        // avg_a = (a+prev_a)/2
+        var avga = this.a.add(preva).scale(0.5);
 
-        this.vx += avgax * delta;
-        this.vy += avgay * delta;
+        // v = v + avg_a*dt
+        this.v = this.v.add(avga.scale(delta));
 
-        this.netfx = 0;
-        this.netfy = 0;
+        this.netf = new Vec2();
     }
 
     this.render = function(ctx)
     {
         ctx.fillStyle = "#000000";
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.arc(this.p.x, this.p.y, this.r, 0, 2*Math.PI);
         ctx.fill();
     }
 }
 
 var orbit_r = 150;
-var init_m = 100;
+var init_m = 0.1;
 
 var gravPoint = new function()
 {
-    this.x = cvs.width/2;
-    this.y = cvs.height/2;
-    this.m = 100000;
+    this.p = new Vec2(cvs.width/2, cvs.height/2);
+    this.m = 10;
 
     this.update = function(delta){}
+
     this.render = function(ctx)
     {
         ctx.fillStyle = "#FF0000";
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 10, 0, 2*Math.PI);
+        ctx.arc(this.p.x, this.p.y, 10, 0, 2*Math.PI);
         ctx.fill();
     }
 }
@@ -75,32 +122,31 @@ var gravPoint = new function()
 var STEPS_PER_FRAME = 1;
 
 var G = 6.646e-11; // N m^2 / kg^2
-var pixels_to_meters = 1/100000*// 100 px = 1 cm
+var meters_to_pixels = 100000;
+var pixels_to_meters = 1/meters_to_pixels; // 100 px = 1 cm
 var circle;
 // initialize circle without polluting namespace
 (function ()
 {
-    var v = Math.sqrt(G*gravPoint.m/orbit_r);
+    var r = orbit_r*pixels_to_meters;
+    var v_mag = Math.sqrt(G*gravPoint.m/r);
 
-    var vx = 0;
-    var vy = v;
-    circle = new Circle(gravPoint.x-orbit_r, gravPoint.y, 50, init_m, vx, vy);
+    var v = new Vec2(0, v_mag*meters_to_pixels);
+
+    circle = new Circle(gravPoint.p.subtract(new Vec2(orbit_r, 0)), 50, init_m, v);
 })();
 
 function update(delta)
 {
-    var deltax = gravPoint.x - circle.x;
-    var deltay = gravPoint.y - circle.y;
-    var rsq = deltax*deltax + deltay*deltay;
+    var diff = gravPoint.p.subtract(circle.p).scale(pixels_to_meters);
+    var rsq = diff.length_squared();
     if (rsq == 0) rsq = 0.1;
     var r = Math.sqrt(rsq);
-    r *= pixels_to_meters;
-    var f = G*gravPoint.m*circle.m/rsq;
+    var f_mag = G*gravPoint.m*circle.m/rsq;
 
-    var fx = f*deltax/r;
-    var fy = f*deltay/r;
+    var f = diff.scale(f_mag*meters_to_pixels/r);
 
-    circle.addForce(fx, fy);
+    circle.addForce(f);
     circle.update(delta);
 }
 
